ff470daf4ccac4298e41614c61e2ecb489d89a60
ihawn <ihawn.pl15@gmail.com>
2022-02-12 14:39:52 -0600

    OP-1 - Models

--- CityBuilder-Main/Assets/Scripts/Classes/ObjectSettings.cs
+++ CityBuilder-Main/Assets/Scripts/Classes/ObjectSettings.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ObjectSettings
 {
     public GameObject Model { get; set; }
+    public Material Material { get; set; }
     public List<GameObject> Children { get; set; }
     public Color Color { get; set; }
 }
